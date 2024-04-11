ef97f637bbd6c2cd45198cc1c781b7c2c3e3ddac
goneck <105847748+goneck@users.noreply.github.com>
2024-04-11 21:17:55 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,18 +1,15 @@
 from collections import deque
 def solution(cards1, cards2, goal):
-    answer = ''
     cards1=deque(cards1)
     cards2=deque(cards2)
     for i in range(len(goal)):
         if len(cards1)>0 and cards1[0]==goal[i]:
-            answer+=cards1.popleft()
-            continue
-        if len(cards2)>0 and cards2[0]==goal[i]:
-            answer+=cards2.popleft()
-            continue
-    if answer!=''.join(goal):
-        return "No"
-    else:
-        return "Yes"
+            cards1.popleft()
+        elif len(cards2)>0 and cards2[0]==goal[i]:
+            cards2.popleft()
+        else:
+            return "No"
+        if i==len(goal)-1:
+            return "Yes"
             
         
