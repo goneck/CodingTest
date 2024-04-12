1549a65d8193d7821b6d58e17fbbaceea889b661
goneck <105847748+goneck@users.noreply.github.com>
2024-04-12 19:56:26 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -2,31 +2,17 @@ from collections import Counter
 def solution(X, Y):
     xnums=Counter(list(X))
     ynums=Counter(list(Y))
-    # for i in range(10):
-    #     xnums[i]=X.count(str(i))
-    # for i in range(10):
-    #     ynums[i]=Y.count(str(i))
+    
     answer=""
+    
     for i in range(9, -1, -1):
         answer+=str(i)*min(xnums[str(i)], ynums[str(i)])
+        
     if answer=="":
         return "-1"
-    elif len(answer)==answer.count("0"):
+    if len(answer)==answer.count("0"):
         return "0"
-    else:
-        return answer
-#     answer=""
-#     for i in range(9, -1, -1):
-#         answer+=str(i)*min(X.count(str(i)), Y.count(str(i)))
-    
-#     if answer=="":
-#         return "-1"
-    
-#     if len(answer) == answer.count("0"):
-#         return "0"
-    
-#     return answer
-        
+    return answer
         
         
         
