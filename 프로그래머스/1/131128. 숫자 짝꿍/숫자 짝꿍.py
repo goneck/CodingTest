e94b00eb0b208162344fc833ecf70400a03e397f
goneck <105847748+goneck@users.noreply.github.com>
2024-04-12 19:53:58 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,38 +1,31 @@
+from collections import Counter
 def solution(X, Y):
-    # X=sorted(X)
-    # Y=sorted(Y)
-    # xnums=dict()
-    # ynums=dict()
+    xnums=Counter(list(X))
+    ynums=Counter(list(Y))
     # for i in range(10):
-    #     cnt=X.count(str(i))
-    #     xnums[i]=cnt
-    #     X=X[cnt:]
+    #     xnums[i]=X.count(str(i))
     # for i in range(10):
-    #     cnt=Y.count(str(i))
-    #     ynums[i]=cnt
-    #     Y=Y[cnt:]
-    # answer=""
-    # for i in range(9, -1, -1):
-    #     if xnums[i]==0 or ynums[i]==0:
-    #         continue
-    #     answer+=str(i)*min(xnums[i], ynums[i])
-    # if answer=="":
-    #     return "-1"
-    # elif int(answer)==0:
-    #     return "0"
-    # else:
-    #     return answer
+    #     ynums[i]=Y.count(str(i))
     answer=""
     for i in range(9, -1, -1):
-        answer+=str(i)*min(X.count(str(i)), Y.count(str(i)))
-    
+        answer+=str(i)*min(xnums[str(i)], ynums[str(i)])
     if answer=="":
         return "-1"
-    
-    if len(answer) == answer.count("0"):
+    elif len(answer)==answer.count("0"):
         return "0"
+    else:
+        return answer
+#     answer=""
+#     for i in range(9, -1, -1):
+#         answer+=str(i)*min(X.count(str(i)), Y.count(str(i)))
     
-    return answer
+#     if answer=="":
+#         return "-1"
+    
+#     if len(answer) == answer.count("0"):
+#         return "0"
+    
+#     return answer
         
         
         
